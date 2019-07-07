4ffe64ae882f73bedd1e552d869747a5f48c122d
teodesson <teodesson@gmail.com>
2019-07-08 00:45:49 +0800

    java.util.Date get/setTime() and toString() examples

---
+++ src/main/java/com/dariawan/datetime/UtilDateGetSetTimeExample.java
@@ -0,0 +1,67 @@
+/**
+ * Code Examples in JDK 8 v1 (https://www.dariawan.com)
+ * Copyright (C) ${year} Dariawan <hello@dariawan.com>
+ *
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ * Under this license, you are free to:
+ * # Share - copy and redistribute the material in any medium or format
+ * # Adapt - remix, transform, and build upon the material for any purpose,
+ *   even commercially.
+ *
+ * The licensor cannot revoke these freedoms
+ * as long as you follow the license terms.
+ *
+ * License terms:
+ * # Attribution - You must give appropriate credit, provide a link to the
+ *   license, and indicate if changes were made. You may do so in any
+ *   reasonable manner, but not in any way that suggests the licensor
+ *   endorses you or your use.
+ * # ShareAlike - If you remix, transform, or build upon the material, you must
+ *   distribute your contributions under the same license as the original.
+ * # No additional restrictions - You may not apply legal terms or
+ *   technological measures that legally restrict others from doing anything the
+ *   license permits.
+ *
+ * Notices:
+ * # You do not have to comply with the license for elements of the material in
+ *   the public domain or where your use is permitted by an applicable exception
+ *   or limitation.
+ * # No warranties are given. The license may not give you all of
+ *   the permissions necessary for your intended use. For example, other rights
+ *   such as publicity, privacy, or moral rights may limit how you use
+ *   the material.
+ *
+ * You may obtain a copy of the License at
+ *   https://creativecommons.org/licenses/by-sa/4.0/
+ *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ */
+package com.dariawan.datetime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UtilDateGetSetTimeExample {
+    
+    public static void main(String[] args) {
+        Date date1 = new Date();
+        System.out.println("date1: " + date1);
+        System.out.println("date1.getTime(): " + date1.getTime());
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	String strDate = "09/04/1980 15:45:20";
+        Date date2; 
+        try {
+            date2 = sdf.parse(strDate);
+            System.out.println("date2: " + date2);
+        } catch (ParseException ex) {
+            System.out.println("ParseException occured: " + ex.getMessage());
+            date2 = new Date();
+        }
+        
+        long time = date2.getTime();
+        date1.setTime(time);
+        System.out.println("after setTime() date1: " + date1);
+    }
+}
---
+++ src/main/java/com/dariawan/datetime/UtilDateToStringExample.java
@@ -0,0 +1,58 @@
+/**
+ * Code Examples in JDK 8 v1 (https://www.dariawan.com)
+ * Copyright (C) ${year} Dariawan <hello@dariawan.com>
+ *
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ * Under this license, you are free to:
+ * # Share - copy and redistribute the material in any medium or format
+ * # Adapt - remix, transform, and build upon the material for any purpose,
+ *   even commercially.
+ *
+ * The licensor cannot revoke these freedoms
+ * as long as you follow the license terms.
+ *
+ * License terms:
+ * # Attribution - You must give appropriate credit, provide a link to the
+ *   license, and indicate if changes were made. You may do so in any
+ *   reasonable manner, but not in any way that suggests the licensor
+ *   endorses you or your use.
+ * # ShareAlike - If you remix, transform, or build upon the material, you must
+ *   distribute your contributions under the same license as the original.
+ * # No additional restrictions - You may not apply legal terms or
+ *   technological measures that legally restrict others from doing anything the
+ *   license permits.
+ *
+ * Notices:
+ * # You do not have to comply with the license for elements of the material in
+ *   the public domain or where your use is permitted by an applicable exception
+ *   or limitation.
+ * # No warranties are given. The license may not give you all of
+ *   the permissions necessary for your intended use. For example, other rights
+ *   such as publicity, privacy, or moral rights may limit how you use
+ *   the material.
+ *
+ * You may obtain a copy of the License at
+ *   https://creativecommons.org/licenses/by-sa/4.0/
+ *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ */
+package com.dariawan.datetime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UtilDateToStringExample {
+    
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	String strDate = "07/07/2019 14:16:55";        
+        try {
+            Date date = sdf.parse(strDate);
+            System.out.println("date: " + date); // implicitely call toString()
+            System.out.println("date toString(): " + date.toString());
+        } catch (ParseException ex) {
+            System.out.println("ParseException occured: " + ex.getMessage());
+        }
+    }
+}
