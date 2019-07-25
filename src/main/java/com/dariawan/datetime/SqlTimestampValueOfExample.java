1b991e7554aca4750ebfdbc2b0f42c775f526d74
teodesson <teodesson@gmail.com>
2019-07-26 00:15:36 +0800

    java.sql.Timestamp init examples, valueOf(), from(), and toInstant() examples

---
+++ src/main/java/com/dariawan/datetime/SqlTimestampValueOfExample.java
@@ -0,0 +1,53 @@
+/**
+ * Code Examples in JDK 8 v1 (https://www.dariawan.com)
+ * Copyright (C) ${year} Dariawan <hello@dariawan.com>
+ *
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ * Under this license, you are free to:
+ * # Share - copy and redistribute the material in any medium or format
+ * # Adapt - remix, transform, and build upon the material for any purpose,
+ *   even commercially.
+ *
+ * The licensor cannot revoke these freedoms
+ * as long as you follow the license terms.
+ *
+ * License terms:
+ * # Attribution - You must give appropriate credit, provide a link to the
+ *   license, and indicate if changes were made. You may do so in any
+ *   reasonable manner, but not in any way that suggests the licensor
+ *   endorses you or your use.
+ * # ShareAlike - If you remix, transform, or build upon the material, you must
+ *   distribute your contributions under the same license as the original.
+ * # No additional restrictions - You may not apply legal terms or
+ *   technological measures that legally restrict others from doing anything the
+ *   license permits.
+ *
+ * Notices:
+ * # You do not have to comply with the license for elements of the material in
+ *   the public domain or where your use is permitted by an applicable exception
+ *   or limitation.
+ * # No warranties are given. The license may not give you all of
+ *   the permissions necessary for your intended use. For example, other rights
+ *   such as publicity, privacy, or moral rights may limit how you use
+ *   the material.
+ *
+ * You may obtain a copy of the License at
+ *   https://creativecommons.org/licenses/by-sa/4.0/
+ *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ */
+package com.dariawan.datetime;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class SqlTimestampValueOfExample {
+    
+    public static void main(String[] args) {
+        Timestamp sqlTimestamp1 = Timestamp.valueOf("1983-07-12 21:30:55.888");
+        System.out.println("SqlTimestamp1: " + sqlTimestamp1);
+        
+        Timestamp sqlTimestamp2 = Timestamp.valueOf(LocalDateTime.of(1980, 4 , 9, 8, 15, 20, 345000000));
+        System.out.println("SqlTimestamp2: " + sqlTimestamp2);
+    }
+}
