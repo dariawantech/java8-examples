75c9d887a68fe60660c7f6fee2dcdb8c46e91261
teodesson <teodesson@gmail.com>
2019-08-02 02:02:47 +0800

    java.util.Date to java.sql.Timestamp example and vice versa

---
+++ src/main/java/com/dariawan/datetime/UtilDateToSqlTimestampExample.java
@@ -0,0 +1,55 @@
+/**
+ * Code Examples in JDK 8 v1 (https://www.dariawan.com)
+ * Copyright (C) ${year} Dariawan <hello@dariawan.com>
+ *
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ * Under this license, you are free to:
+ * # Share - copy and redistribute the material in any medium or format
+ * # Adapt - remix, transform, and build upon the material for any purpose,
+ *   even commercially.
+ *
+ * The licensor cannot revoke these freedoms
+ * as long as you follow the license terms.
+ *
+ * License terms:
+ * # Attribution - You must give appropriate credit, provide a link to the
+ *   license, and indicate if changes were made. You may do so in any
+ *   reasonable manner, but not in any way that suggests the licensor
+ *   endorses you or your use.
+ * # ShareAlike - If you remix, transform, or build upon the material, you must
+ *   distribute your contributions under the same license as the original.
+ * # No additional restrictions - You may not apply legal terms or
+ *   technological measures that legally restrict others from doing anything the
+ *   license permits.
+ *
+ * Notices:
+ * # You do not have to comply with the license for elements of the material in
+ *   the public domain or where your use is permitted by an applicable exception
+ *   or limitation.
+ * # No warranties are given. The license may not give you all of
+ *   the permissions necessary for your intended use. For example, other rights
+ *   such as publicity, privacy, or moral rights may limit how you use
+ *   the material.
+ *
+ * You may obtain a copy of the License at
+ *   https://creativecommons.org/licenses/by-sa/4.0/
+ *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ */
+package com.dariawan.datetime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class UtilDateToSqlTimestampExample {
+    
+    public static void main(String[] args) {
+        java.util.Date utilDate = new java.util.Date();
+        System.out.println("java.util.Date time    : " + utilDate);
+        java.sql.Timestamp sqlTS = new java.sql.Timestamp(utilDate.getTime());
+        System.out.println("java.sql.Timestamp time: " + sqlTS);
+        
+        DateFormat df = new SimpleDateFormat("dd/MM/YYYY hh:mm:ss:SSS");
+        System.out.println("Date formatted         : " + df.format(utilDate));
+    }
+}
